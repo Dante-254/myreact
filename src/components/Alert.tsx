49f38481f48a29db136b6267cce315c218103631
Dante-254 <giciadaniel575@gmail.com>
2025-05-04 14:41:21 +0300

    4-may-25 commit

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,17 +1,23 @@
 import { ReactNode } from "react";
 
-interface alertProps{
-    children : ReactNode;
-    onClose: () => void
+interface alertProps {
+  children: ReactNode;
+  onClose: () => void;
 }
 
-
-const Alert = ({children, onClose}: alertProps) => {
+const Alert = ({ children, onClose }: alertProps) => {
   return (
-    <div className="alert alert-info alert-dismissible fade show">{children}
-    <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={onClose}></button>
+    <div className="alert alert-info alert-dismissible fade show">
+      {children}
+      <button
+        type="button"
+        className="btn-close"
+        data-bs-dismiss="alert"
+        aria-label="Close"
+        onClick={onClose}
+      ></button>
     </div>
-  )
-}
+  );
+};
 
-export default Alert
+export default Alert;
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,17 @@
-import { ReactNode } from 'react'
+import { ReactNode } from "react";
 
-interface buttonProps{
+interface buttonProps {
   children: ReactNode;
-  color?: string
-  onClick: () => void
+  color?: string;
+  onClick: () => void;
 }
 
-const Button = ({children, color='primary',  onClick}: buttonProps) => {
+const Button = ({ children, color = "primary", onClick }: buttonProps) => {
   return (
-    <button type="button" className={"btn btn-"+color} onClick={onClick}>{children}</button>
-  )
-}
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
